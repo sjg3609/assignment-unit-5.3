50119ce1ccfb46a748a562cf7cc2c84ac7072eab
Sam Gossie <sjg3609@gmail.com>
2023-02-02 09:09:01 -0600

    Attempting more code to addCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,10 +2,13 @@ console.log('***** Music Collection *****')
 
 let collection = [];
 
-function addToCollection( array ) {
+function addToCollection( title, artist, yearPublished ) {
     let collection = {
         title: '',
         artist: '',
         yearPublished: '',
     }
-}
+    return collection;
+} 
+
+console.log(addToCollection('Four', 'One Direction', '2014'));
