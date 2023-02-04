3c751b54c90d0450a0c3309ffa286c5c2df45922
Sam Gossie <sjg3609@gmail.com>
2023-02-03 20:08:50 -0600

    Finished addCollection function and started showCollection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -3,12 +3,28 @@ console.log('***** Music Collection *****')
 let collection = [];
 
 function addToCollection( title, artist, yearPublished ) {
-    let collection = {
-        title: '',
-        artist: '',
-        yearPublished: '',
-    }
-    return collection;
+    let albums = {
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished,
+    };
+    return albums;
 } 
 
 console.log(addToCollection('Four', 'One Direction', '2014'));
+
+collection.push(addToCollection('Four', 'One Direction', '2014'));
+collection.push(addToCollection('Midnights', 'Taylor Swift', '2022'));
+collection.push(addToCollection('Acid Rap', 'Chance the Rapper', '2013'));
+collection.push(addToCollection('Because the Internet', 'Childisn Gambino', '2013'));
+collection.push(addToCollection('Coloring Book', 'Chance the Rapper', '2016'));
+collection.push(addToCollection('Jazz', 'Queen', '1978'));
+
+console.log(collection);
+
+function showCollection( array ) {
+    console.log(array);
+    for (let x of collection) {
+        
+    }
+}
