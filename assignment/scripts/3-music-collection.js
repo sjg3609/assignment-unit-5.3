930000ae48345c72831bb1ce75a17372b6a6678c
Sam Gossie <sjg3609@gmail.com>
2023-02-05 17:10:47 -0600

    attempting finyByArtist function and cleaned up some syntax

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -19,10 +19,10 @@ console.log(addToCollection('Because the Internet', 'Childisn Gambino', '2013'))
 console.log(addToCollection('Coloring Book', 'Chance the Rapper', '2016'));
 console.log(addToCollection('Jazz', 'Queen', '1978'));
 
-console.log(collection);
+console.log('Display collection after adding albums: ', collection);
 
 function showCollection( array ) {
-    console.log(array);
+    console.log('in showCollection function: ', array);
     for ( let i = 0; i < array.length; i++) {
             console.log(`${collection[i].title} by ${collection[i].artist}, published in ${collection[i].yearPublished}`);
     }
@@ -31,14 +31,14 @@ function showCollection( array ) {
 showCollection(collection);
 
 function findByArtist( artist ) {
+    console.log('In findByArtist function:', artist)
     let results = [];
-    for (let artist of collection) {
-        if (artist === collection) {
+    for (let artistSearch of collection) {
+        if (artistSearch === collection.length) {
             results.push(artist);
-            return results;
         } 
         
-    }
+    } return results;
 }
 
 console.log(findByArtist('Taylor Swift'));
